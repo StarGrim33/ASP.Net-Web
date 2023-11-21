d742700857d979c2717f235d60cd2995bb3005e7
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-11-21 18:40:24 +0300

    Created project

---
+++ Diamond-Cleaning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Diamond_Cleaning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
