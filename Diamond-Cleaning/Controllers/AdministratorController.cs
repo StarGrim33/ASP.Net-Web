ea90cc0dcd10f2ead53fcaa7b0f0de7aa04af4ec
StarGrim33 <prosky33@gmail.com>
2023-12-06 10:05:30 +0300

    added admin panel

---
+++ Diamond-Cleaning/Controllers/AdministratorController.cs
@@ -0,0 +1,44 @@
+﻿using Diamond_Cleaning.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diamond_Cleaning.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        private IServicesRepository _servicesRepository;
+
+        public AdministratorController(IServicesRepository servicesRepository)
+        {
+            _servicesRepository = servicesRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetOrders()
+        {
+            return View();
+        }
+
+        public IActionResult GetUsers()
+        {
+            return View();
+        }
+
+        public IActionResult GetRoles()
+        {
+            return View();
+        }
+
+        public IActionResult GetProducts()
+        {
+            var services = _servicesRepository.GetServices();
+
+            if(services != null)
+                return View(services);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
